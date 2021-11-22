b9aa4d86b971b5d2dfb450b6d662d5421f1da94d
lakimexo <kimmv1975@gmail>
2021-11-22 18:12:14 +0600

    Completed assingnment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+const dodger = document.getElementById("dodger"); 
+console.log(dodger);
+document.addEventListener('keydown', function(e) {
+    console.log(e.which)
+  })
+
+
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); 
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(leftNumbers, 10);
+  
+    if (right < 360) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  } 
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight" ) {
+      moveDodgerRight();
+    }
+  });
